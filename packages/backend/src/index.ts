a60a7110ddc025463c2bc389c7ed90dc8d0be446
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-02-18 01:17:26 +0200

    Move websocket and http server to single file

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,18 +1,28 @@
 import express from 'express';
-import wsServer from './ws-server';
+// import wsServer from './ws-server';
+import { Server } from 'ws';
 
 const PORT = 3001;
 
 const app = express();
 const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+const wsserver = new Server({ server });
 
 enum Event {
   UPGRADE = 'upgrade',
   CONNECTION = 'connection'
 }
 
-server.on(Event.UPGRADE, (request, socket, head) => {
-  wsServer.handleUpgrade(request, socket, head, socket => {
-    wsServer.emit(Event.CONNECTION, socket, request);
+wsserver.on('connection', (websocket, request) => {
+  console.log('Incoming websocket connection', { websocket, request });
+  websocket.on('message', (msg: any) => {
+    console.log('Incoming msg', msg);
   });
+  console.log('Set up listener');
 });
+
+// server.on(Event.UPGRADE, (request, socket, head) => {
+//   wsserver.handleUpgrade(request, socket, head, socket => {
+//     wsserver.emit(Event.CONNECTION, socket, request);
+//   });
+// });
