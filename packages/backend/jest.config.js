44248a251eb827f87f40d0ee83d913000fa78098
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-02-18 00:01:18 +0200

    Set up testing in backend

---
+++ packages/backend/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node'
+};
+
+// module.exports = {
+//   transform: { '^.+\\.ts?$': 'ts-jest' },
+//   testEnvironment: 'node',
+//   testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+//   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+// };
---
+++ packages/backend/src/tests/utils.test.ts
@@ -0,0 +1,5 @@
+import * as utils from '../utils';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(utils.add(1, 2)).toBe(3);
+});
