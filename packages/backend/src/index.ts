b75b4a8868d9a2cc9d6086cccd673e2a0874df41
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-02-17 20:23:43 +0200

    backend: make sure local imports work

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import * as utils from './utils';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello whiteboard!');
+  res.send(`Hello whiteboard! ${utils.add(3, 4)}`);
 });
 
 app.listen(port, () => {
