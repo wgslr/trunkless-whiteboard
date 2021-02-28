be0df5b71b993699c8fac3139a01261131a98edd
haraldholmstrom <harald.holmstrom@gmail.com>
2021-02-28 17:04:23 +0200

    Added README, Editor component

---
+++ packages/client/src/editor/components/Editor.tsx
@@ -0,0 +1,5 @@
+// TODO
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import render from '../render';
+import bitmap from '../whiteboard';
+// import Cursor from './Cursor'
