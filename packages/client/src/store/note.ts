2865b65fea2ca616ed08b3ef82d6b520b5e88c83
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-03-13 01:43:36 +0200

    Store multiple note drafts

--- packages/client/src/store/note.ts
+++ packages/client/src/store/note.ts
@@ -1,25 +1,43 @@
-import type { Note, Coordinates } from '../types';
+import { v4 } from 'uuid';
+import type { Note, Coordinates, UUID } from '../types';
+
+type Patch = {
+  changeId: UUID;
+  newState: Note;
+};
 
 type NoteTimeline = {
   commited: Note | null;
-  draft: Note | null;
+  drafts: Patch[];
 };
+
+export const getNewestLocalState = (nt: NoteTimeline): Note | null => {
+  if (nt.drafts.length > 0) {
+    return nt.drafts[nt.drafts.length - 1].newState;
+  } else {
+    return nt.commited;
+  }
+};
+
+const newPatch = (newState: Note) => ({ changeId: v4(), newState });
 
 export const setDraftText = (
   nt: NoteTimeline,
   newText: string
 ): NoteTimeline => {
-  const current = nt.draft ?? nt.commited;
+  const current = getNewestLocalState(nt);
   if (!current) {
-    return nt;
     console.error('moveNote called on Timeline with both states null');
+    return nt;
   } else {
     return {
       ...nt,
-      draft: {
-        ...current,
-        text: newText
-      }
+      drafts: nt.drafts.concat(
+        newPatch({
+          ...current,
+          text: newText
+        })
+      )
     };
   }
 };
@@ -28,14 +46,14 @@ export const moveNote = (
   nt: NoteTimeline,
   newPos: Coordinates
 ): NoteTimeline => {
-  const current = nt.draft ?? nt.commited;
+  const current = nt.drafts ?? nt.commited;
   if (!current) {
     return nt;
     console.error('moveNote called on Timeline with both states null');
   } else {
     return {
       ...nt,
-      draft: {
+      drafts: {
         ...current,
         position: newPos
       }
