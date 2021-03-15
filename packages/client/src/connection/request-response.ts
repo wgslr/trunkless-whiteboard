8fd24413424504c300b4634235fb1aca13916944
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-03-15 19:14:17 +0200

    WIP request-response service

--- packages/client/src/connection/request-response.ts
+++ packages/client/src/connection/request-response.ts
@@ -4,11 +4,13 @@ import {
 } from '../protocol/protocol';
 import { ProtobufSocketClient } from './protobuf-client';
 import * as uuid from 'uuid';
+import { EventSharp } from '@material-ui/icons';
 
 type Callback = (message: ServerToClientMessage['body'] | 'TIMEOUT') => void;
 
 export class RequestResponseService {
-  private messageIdToCallback: Map<string, Callback> = new Map();
+  /** Map from a message id to response callback */
+  // private messageIdToCallback: Map<string, Callback> = new Map();
 
   constructor(private protobufClient: ProtobufSocketClient) {}
 
@@ -16,16 +18,23 @@ export class RequestResponseService {
     const message = { messsageId: uuid.v4(), body };
     this.protobufClient.send(message);
 
-    const eventHandler = ({ body }: Required<ServerToClientMessage>) => {
-      // const data =
-      // if ('causedBy' in data) {
-      //   const causedBy = data.causedBy;
-      // }
-      // causedBy
-    };
-
     if (callback) {
+      const eventHandler = (serverMessage: ServerToClientMessage) => {
+        if (
+          serverMessage.causedBy &&
+          serverMessage.causedBy == message.messsageId
+        ) {
+          const callback = this.messageIdToCallback.get(message.causedBy);
+          if (callback) {
+            callback(message.body);
+          }
+        }
+      };
+
       // this.messageIdToCallback.set(messageId, callback);
+
+      this.protobufClient.addListener('message', eventHandler);
+      this.protobufClient.
     }
   }
 }
