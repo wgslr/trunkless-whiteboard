c2452393bbcbed4fa6f3787970fd43d256b7b7ca
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-03-17 16:11:41 +0200

    Separate Canvas from Editor
    This avoids rerendering <Stickies/> when lines change and improves code
    readability.

---
+++ packages/client/src/editor/components/Canvas.tsx
@@ -0,0 +1,113 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { addNote } from '../../controllers/note-controller';
+import { useEffectiveLines } from '../../store/hooks';
+import { coordToNumber } from '../../utils';
+import render from '../render';
+import { modeState } from '../state';
+import {
+  appendErase,
+  appendLine,
+  finishErase,
+  finishLine,
+  startErase,
+  startLine
+} from '../whiteboard';
+
+const Canvas = (props: { x: number; y: number }) => {
+  const effectiveLines = [...useEffectiveLines().values()];
+  const canvas = useRef<HTMLCanvasElement>(null);
+
+  const getCtx = () => {
+    return canvas.current !== null ? canvas.current.getContext('2d') : null;
+  };
+
+  const [mode, setMode] = useRecoilState(modeState);
+  const [pointerDown, setPointerDown] = useState(false);
+
+  const handlePointerDown = useCallback(
+    (event: PointerEvent) => {
+      setPointerDown(true);
+      const point = { x: event.x, y: event.y };
+      if (mode === 'draw') {
+        startLine(coordToNumber(point));
+      } else if (mode === 'erase') {
+        startErase(coordToNumber(point));
+      } else if (mode === 'note') {
+        addNote(point);
+      }
+    },
+    [mode]
+  );
+
+  const handlePointerUp = useCallback(() => {
+    setPointerDown(false);
+    if (mode === 'draw') {
+      finishLine();
+    } else if (mode === 'erase') {
+      finishErase();
+    }
+  }, [mode]);
+
+  const handlePointerMove = useCallback(
+    (event: PointerEvent) => {
+      if (event.target !== canvas.current) {
+        return;
+      }
+      const point = coordToNumber({ x: event.x, y: event.y });
+
+      if (pointerDown) {
+        if (mode === 'draw') {
+          appendLine(point);
+        } else if (mode === 'erase') {
+          appendErase(point);
+        }
+      }
+    },
+    [mode, pointerDown]
+  );
+
+  useEffect(() => {
+    if (canvas.current === null) {
+      return;
+    }
+  }, []);
+
+  useEffect(() => {
+    if (canvas.current === null) {
+      return;
+    }
+
+    const canvasElem = canvas.current;
+
+    canvasElem.addEventListener('pointermove', handlePointerMove);
+    canvasElem.addEventListener('pointerdown', handlePointerDown);
+    canvasElem.addEventListener('pointerup', handlePointerUp);
+
+    return () => {
+      canvasElem.removeEventListener('pointermove', handlePointerMove);
+      canvasElem.removeEventListener('pointerdown', handlePointerDown);
+      canvasElem.removeEventListener('pointerup', handlePointerUp);
+    };
+  }, [handlePointerMove, handlePointerDown, handlePointerUp]);
+
+  // Main render function
+  useEffect(() => {
+    render(getCtx()!, canvas.current!, [...effectiveLines.values()]);
+  }, [effectiveLines]);
+
+  return (
+    <canvas
+      ref={canvas}
+      height={props.y}
+      width={props.x}
+      style={{ border: '1px solid black', backgroundColor: 'white' }}
+    ></canvas>
+  );
+  // <Cursor canvas={canvas.current!}></Cursor>
+
+  // Layered canvas
+  // https://github.com/federicojacobi/layeredCanvas
+};
+
+export default Canvas;
--- packages/client/src/editor/components/Editor.tsx
+++ packages/client/src/editor/components/Editor.tsx
@@ -1,109 +1,10 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { useRecoilState } from 'recoil';
-import { addNote } from '../../controllers/note-controller';
-import { useEffectiveLines } from '../../store/hooks';
-import { coordToNumber } from '../../utils';
-import render from '../render';
-import { modeState } from '../state';
-import {
-  appendErase,
-  appendLine,
-  finishErase,
-  finishLine,
-  startErase,
-  startLine,
-  undo
-} from '../whiteboard';
+import React from 'react';
+import Canvas from './Canvas';
 import Stickies from './Stickies';
 import Tools from './Tools';
 import UndoTool from './UndoTool';
 
 const Editor = (props: { x: number; y: number }) => {
-  const effectiveLines = [...useEffectiveLines().values()];
-  const canvas = useRef<HTMLCanvasElement>(null);
-
-  const getCtx = () => {
-    return canvas.current !== null ? canvas.current.getContext('2d') : null;
-  };
-
-  const [mode, setMode] = useRecoilState(modeState);
-  const [pointerDown, setPointerDown] = useState(false);
-
-  const handlePointerDown = useCallback(
-    (event: PointerEvent) => {
-      setPointerDown(true);
-      const point = { x: event.x, y: event.y };
-      if (mode === 'draw') {
-        startLine(coordToNumber(point));
-      } else if (mode === 'erase') {
-        startErase(coordToNumber(point));
-      } else if (mode === 'note') {
-        addNote(point);
-      }
-    },
-    [mode]
-  );
-
-  const handlePointerUp = useCallback(() => {
-    setPointerDown(false);
-    if (mode === 'draw') {
-      finishLine();
-    } else if (mode === 'erase') {
-      finishErase();
-    }
-  }, [mode]);
-
-  const handlePointerMove = useCallback(
-    (event: PointerEvent) => {
-      if (event.target !== canvas.current) {
-        return;
-      }
-      const point = coordToNumber({ x: event.x, y: event.y });
-
-      if (pointerDown) {
-        if (mode === 'draw') {
-          appendLine(point);
-        } else if (mode === 'erase') {
-          appendErase(point);
-        }
-      }
-    },
-    [mode, pointerDown]
-  );
-
-  const renderUndo = () => {
-    undo();
-  };
-
-  useEffect(() => {
-    if (canvas.current === null) {
-      return;
-    }
-  }, []);
-
-  useEffect(() => {
-    if (canvas.current === null) {
-      return;
-    }
-
-    const canvasElem = canvas.current;
-
-    canvasElem.addEventListener('pointermove', handlePointerMove);
-    canvasElem.addEventListener('pointerdown', handlePointerDown);
-    canvasElem.addEventListener('pointerup', handlePointerUp);
-
-    return () => {
-      canvasElem.removeEventListener('pointermove', handlePointerMove);
-      canvasElem.removeEventListener('pointerdown', handlePointerDown);
-      canvasElem.removeEventListener('pointerup', handlePointerUp);
-    };
-  }, [handlePointerMove, handlePointerDown, handlePointerUp]);
-
-  // Main render function
-  useEffect(() => {
-    render(getCtx()!, canvas.current!, [...effectiveLines.values()]);
-  }, [effectiveLines]);
-
   return (
     <div
       id="editor"
@@ -114,15 +15,10 @@ const Editor = (props: { x: number; y: number }) => {
     >
       <div>
         <Tools />
-        <UndoTool onClick={renderUndo} />
+        <UndoTool onClick={() => {}} />
       </div>
       <Stickies />
-      <canvas
-        ref={canvas}
-        height={props.y}
-        width={props.x}
-        style={{ border: '1px solid black', backgroundColor: 'white' }}
-      ></canvas>
+      <Canvas {...props} />
     </div>
   );
   // <Cursor canvas={canvas.current!}></Cursor>
