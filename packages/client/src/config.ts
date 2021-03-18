fa1b114fb7295fae23f7d7c8c9c53bfb26a54414
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-03-18 22:02:38 +0200

    Set websocket url based on current page url

--- packages/client/src/config.ts
+++ packages/client/src/config.ts
@@ -1,4 +1,5 @@
 /** After this time we stop looking for response among incoming messages */
 export const SERVER_RESPONSE_TIMEOUT = 10_000;
 
-export const SERVER_URL = process.env.FULL_WEBSOCKET_URL || 'ws://localhost:3001/ws';
+const { location } = window;
+export const SERVER_URL = `ws://${location.host}/ws`;
