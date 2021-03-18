bf0b7763502462854d1e393c621e0c12891e7ba5
Axel Neergaard <axel.git@neergaard.dev>
2021-03-18 20:12:26 +0200

    Set websocket URL through environmental variable

--- packages/client/src/config.ts
+++ packages/client/src/config.ts
@@ -1,5 +1,4 @@
 /** After this time we stop looking for response among incoming messages */
 export const SERVER_RESPONSE_TIMEOUT = 10_000;
 
-// TODO: set as env variable
-export const SERVER_URL = 'ws://localhost:3001/ws';
+export const SERVER_URL = process.env.FULL_WEBSOCKET_URL || 'ws://localhost:3001/ws';
