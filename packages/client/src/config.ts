f09bf7858aa7671c7af147d45614525a356ef9f2
Axel Neergaard <axel.git@neergaard.dev>
2021-03-19 19:41:05 +0200

    Revert "Downgrade Secure WebSocket protocol to normal WebSocket in production"
    This reverts commit a651b943e39a679fdc25b7e8d7f83f3c464c9a68.

--- packages/client/src/config.ts
+++ packages/client/src/config.ts
@@ -1,4 +1,9 @@
 /** After this time we stop looking for response among incoming messages */
 export const SERVER_RESPONSE_TIMEOUT = 10_000;
 
-export const SERVER_URL = `ws://${window.location.host}/ws`;
+const get_ws_url = () => {
+  const location = window.location;
+  const protocol = location.protocol === 'https:' ? 'wss:' : 'ws:';
+  return `${protocol}//${location.host}/ws`;
+};
+export const SERVER_URL = get_ws_url();
