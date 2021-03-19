245c2a038665b16906c93849979417692f52e241
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-03-19 20:19:47 +0200

    Remove unused variables and imports

--- packages/client/src/editor/components/StickyNote.tsx
+++ packages/client/src/editor/components/StickyNote.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState, useRef } from 'react';
-import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
 import DeleteIcon from '@material-ui/icons/Delete';
-import { UUID, Note } from '../../types';
+import React from 'react';
+import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
+import { Note, UUID } from '../../types';
 
 interface NoteProps {
   save: (id: UUID, content: string) => void;
@@ -22,16 +22,12 @@ const StickyNote: React.FunctionComponent<NoteProps> = props => {
     props.delete(note.id);
   };
 
-  const style = {
-    top: note.position.y,
-    left: note.position.x
-  };
-
   return (
-    <Draggable 
-      position={{x: note.position.x, y: note.position.y}}
-      onDrag={(e,d) => onMove(e,d)}
-      onStop={(e,d) => onMove(e,d)}>
+    <Draggable
+      position={{ x: note.position.x, y: note.position.y }}
+      onDrag={(e, d) => onMove(e, d)}
+      onStop={(e, d) => onMove(e, d)}
+    >
       <div className="stickyNote">
         <textarea
           onChange={e => props.save(note.id, e.target.value)}
