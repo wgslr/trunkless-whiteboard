9858365332a3ec6bac321a1b52c4136dc12bab6b
Axel Neergaard <axel.git@neergaard.dev>
2021-03-20 21:03:17 +0200

    Cleanup UserList.tsx

--- packages/client/src/editor/components/UserList.tsx
+++ packages/client/src/editor/components/UserList.tsx
@@ -4,19 +4,22 @@ import { usersState, User } from '../../store/users';
 import Button from '@material-ui/core/Button';
 import AcceptIcon from '@material-ui/icons/Done';
 import DenyIcon from '@material-ui/icons/Clear';
+import { approveUser } from '../../controllers/auth-controller';
 
 const UserDisplay = ({ user }: { user: User }) => (
   <li key={user.id}>{user.username}</li>
 );
 
-const PendingUser = ({ user }: { user: User }) => {
+const PendingUserDisplay = ({ user }: { user: User }) => {
+  const handleApprove = () => approveUser(user.id);
+  const handleDeny = () => console.log('DENY');
   return (
     <>
       <UserDisplay user={user} />
-      <Button onClick={() => console.log('CLICKED')} variant="contained">
+      <Button onClick={handleApprove} variant="contained">
         <AcceptIcon />
       </Button>
-      <Button onClick={() => console.log('CLICKED')} variant="contained">
+      <Button onClick={handleDeny} variant="contained">
         <DenyIcon />
       </Button>
     </>
@@ -36,7 +39,7 @@ const UserList = () => {
       <h2>Pending users</h2>
       <ul>
         {users.pending.map(u => (
-          <PendingUser user={u} />
+          <PendingUserDisplay user={u} />
         ))}
       </ul>
     </div>
