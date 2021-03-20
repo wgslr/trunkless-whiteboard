d4088b226c28c867ed1219437be15d68f39ff51b
Wojciech Geisler <wojciech.geisler@gmail.com>
2021-03-20 13:47:28 +0200

    Add input validation to whiteboardId input form

--- packages/client/src/editor/components/WhiteboardPrompt.tsx
+++ packages/client/src/editor/components/WhiteboardPrompt.tsx
@@ -1,8 +1,10 @@
 import React, { FormEvent, useState } from 'react';
 import * as authController from '../../controllers/auth-controller';
+import * as uuid from 'uuid';
 
 export const WhiteboardPrompt: React.FunctionComponent = () => {
   const [whiteboardId, setWhiteboardId] = useState('');
+  const isIdValid = uuid.validate(whiteboardId);
   const handleCreate = (e: FormEvent) => {
     e.preventDefault();
     authController.createWhiteboard();
@@ -18,10 +20,18 @@ export const WhiteboardPrompt: React.FunctionComponent = () => {
       </form>
       <form onSubmit={handleJoin}>
         <input
+          style={{ width: '36ch' }}
+          maxLength={36}
+          minLength={36}
           value={whiteboardId}
           onChange={e => setWhiteboardId(e.target.value)}
         />
-        <input type="submit" value="Join whiteboard" />
+        <input
+          type="submit"
+          value="Join whiteboard"
+          disabled={!whiteboardId || !isIdValid}
+        />
+        <div>{whiteboardId && !isIdValid ? 'Invalid UUID' : ''}</div>
       </form>
     </div>
   );
