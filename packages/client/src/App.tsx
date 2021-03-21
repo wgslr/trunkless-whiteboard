58e1e212cb3df3cb07718fdbee445703cec65ecb
Axel Neergaard <axel.git@neergaard.dev>
2021-03-21 12:37:19 +0200

    Add Alerts component

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -4,11 +4,13 @@ import './App.css';
 import ServerContext, { contextValue } from './connection/ServerContext';
 import Lifecycle from './editor/components/Lifecycle';
 import Topbar from './Topbar';
+import Alerts from './Alerts';
 
 const topBarHeight = 60;
 
 const App = () => (
   <div className="App">
+    <Alerts />
     <Topbar h={topBarHeight} />
     <RecoilRoot>
       <ServerContext.Provider value={contextValue}>
