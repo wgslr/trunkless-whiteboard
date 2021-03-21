7b8fd910e9fc7eb1a1ae4a45aeb25c6a99834536
Axel Neergaard <axel.git@neergaard.dev>
2021-03-21 23:12:33 +0200

    Run client image-controller.ts through Prettier

--- packages/client/src/controllers/image-controller.ts
+++ packages/client/src/controllers/image-controller.ts
@@ -8,34 +8,30 @@ import {
 } from '../connection/messages';
 import { reqResponseService } from '../connection/ServerContext';
 import { ClientToServerMessage } from '../protocol/protocol';
-import {
-  discardPatch,
-  localAddImage,
-  localUpdatePos
-} from '../store/images';
+import { discardPatch, localAddImage, localUpdatePos } from '../store/images';
 import type { Coordinates, Img } from '../types';
 
 export const addImage = (position: Coordinates, data: Uint8Array) => {
-    const image = { id: v4(), position, data };
-    const patchId = localAddImage(image);
-    const body: ClientToServerMessage['body'] = makeCreateImageMessage(image);
-    reqResponseService.send(body, () => {
-        // No need to check the response.
-        // If success - the change is already incorporated into
-        // state sent by server. If not, discarding the patch
-        // undoes the changes.
-        discardPatch(image.id, patchId);
-    });
+  const image = { id: v4(), position, data };
+  const patchId = localAddImage(image);
+  const body: ClientToServerMessage['body'] = makeCreateImageMessage(image);
+  reqResponseService.send(body, () => {
+    // No need to check the response.
+    // If success - the change is already incorporated into
+    // state sent by server. If not, discarding the patch
+    // undoes the changes.
+    discardPatch(image.id, patchId);
+  });
 };
 
 export const updateImagePosition = (
-    figureId: Img['id'],
-    newPos: Coordinates 
+  figureId: Img['id'],
+  newPos: Coordinates
 ): void => {
-    const patchId = localUpdatePos(figureId, newPos);
-    const body: ClientToServerMessage['body'] = makeUpdateImagePosMessage(
-        figureId,
-        newPos
-    );
-    reqResponseService.send(body, () => discardPatch(figureId, patchId));
-}
+  const patchId = localUpdatePos(figureId, newPos);
+  const body: ClientToServerMessage['body'] = makeUpdateImagePosMessage(
+    figureId,
+    newPos
+  );
+  reqResponseService.send(body, () => discardPatch(figureId, patchId));
+};
