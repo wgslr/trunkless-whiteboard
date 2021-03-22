32640fd1bca41c98be09eedd4d57293d4aa72ab4
Axel Neergaard <axel.git@neergaard.dev>
2021-03-22 15:34:13 +0200

    Cleanup client Editor.tsx

--- packages/client/src/editor/components/Editor.tsx
+++ packages/client/src/editor/components/Editor.tsx
@@ -5,28 +5,21 @@ import Stickies from './Stickies';
 import Tools from './Tools';
 import UndoTool from './UndoTool';
 
-const Editor = (props: { x: number; y: number }) => {
-  return (
-    <div
-      id="editor"
-      style={{
-        height: props.y,
-        width: props.x
-      }}
-    >
-      <div>
-        <Tools />
-        <UndoTool onClick={undo} />
-      </div>
-      <Stickies />
-      <Canvas {...props} />
+const Editor = (props: { x: number; y: number }) => (
+  <div
+    id="editor"
+    style={{
+      height: props.y,
+      width: props.x
+    }}
+  >
+    <div>
+      <Tools />
+      <UndoTool onClick={undo} />
     </div>
-
-  );
-  // <Cursor canvas={canvas.current!}></Cursor>
-
-  // Layered canvas
-  // https://github.com/federicojacobi/layeredCanvas
-};
+    <Stickies />
+    <Canvas {...props} />
+  </div>
+);
 
 export default Editor;
